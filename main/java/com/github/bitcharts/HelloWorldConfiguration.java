7fe2a12c3cc1d71fd69f3e7535f941233c9b3761
Radu Miron <radu.miron@hpe.com>
2016-10-28 18:17:44 +0300

    first Hello World REST service

---
+++ main/java/com/github/bitcharts/HelloWorldConfiguration.java
@@ -0,0 +1,14 @@
+package com.github.bitcharts; /**
+ * Created by mironr on 10/27/2016.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldConfiguration.class, args);
+    }
+
+}
