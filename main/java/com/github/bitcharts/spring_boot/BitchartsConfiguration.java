10852d722b086a7883cc7a941261d332f78cc125
Radu Miron <radu.miron.iulian@gmail.com>
2017-01-08 16:57:42 +0200

    Optimizing imports

--- main/java/com/github/bitcharts/spring_boot/BitchartsConfiguration.java
+++ main/java/com/github/bitcharts/spring_boot/BitchartsConfiguration.java
@@ -1,12 +1,10 @@
 package com.github.bitcharts.spring_boot; /**
  * Created by mironr on 10/27/2016.
  */
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.bitcharts.model.rest.CurrencyMixIn;
-import com.github.bitcharts.model.rest.MyModule;
 import com.github.bitcharts.settings.Global;
-
 import org.knowm.xchange.currency.Currency;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
