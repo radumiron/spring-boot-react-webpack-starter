3d1a5b987d3941019335fca5eaca2bf6180c447d
Radu Miron <radu.miron@hpe.com>
2017-08-04 14:40:25 +0300

    change proxy settings to HPE

--- main/java/com/github/bitcharts/settings/Global.java
+++ main/java/com/github/bitcharts/settings/Global.java
@@ -4,7 +4,7 @@ package com.github.bitcharts.settings;
  * Created by mironr on 10/31/2016.
  */
 public class Global {
-  public static final String PROXY_SERVER_HOST = "proxy.bbn.hp.com";
+  public static final String PROXY_SERVER_HOST = "web-proxy.bbn.hpecorp.net";
   public static final String PROXY_SERVER_PORT = "8080";
 
 }
