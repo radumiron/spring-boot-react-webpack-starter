16a194e86683013b2815b316b49221b0ce23d792
Radu Miron <radu.miron@hpe.com>
2018-04-02 15:56:01 +0300

    playing with components

--- src/main/frontend/app/index.js
+++ src/main/frontend/app/index.js
@@ -11,12 +11,27 @@ class Main extends React.Component {
         this.state = {markets: []};
     }
 
+	componentWillMount() {
+        let years = ["2014", "2015", "2016", "2017", "2018"];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let sheets = ["Rate", "Mancare", "Casa", "Oras", "Copil", "Garsoniera", "Anexe", "Total"];
+        this.setState({years});
+        this.setState({months});
+        this.setState({sheets});
+    }
+
+
     render() {
     	return (
     		<div>
 				<h1 className="testblue">App working with hot loading</h1>
 				<div>
-					<DrowdownList/>
+					<DrowdownList
+					data={this.state.years}/>
+					<DrowdownList
+						data={this.state.months}/>
+					<DrowdownList
+						data={this.state.sheets}/>
 				</div>
 			</div>
 		);
